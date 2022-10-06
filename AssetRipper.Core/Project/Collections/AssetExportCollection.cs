dzgreeno/AssetRipper-DzGreen

commit a40071f0dadfb7af26c281f26e8aca5f0a42e5d3

Use full path in AssetExportCollection

AssetRipper.Core/Project/Collections/AssetExportCollection.cs
@@ -29,7 +29,8 @@ namespace AssetRipper.Core.Project.Collections
 					: Asset.OriginalName.Length > 0
 						? Asset.OriginalName
 						: Asset.AssetClassName;
-				string resourcePath = FileUtils.FixInvalidNameCharacters(Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}"));
+				string resourcePath = Path.GetFullPath(FileUtils.FixInvalidNameCharacters(
+					Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
 				subPath = Path.GetDirectoryName(resourcePath)!;
 				string resFileName = Path.GetFileName(resourcePath);
 				fileName = GetUniqueFileName(subPath, resFileName);

commit 560c399f3d3bd7ab9909dddaf35ccc8afe1d036c

Fix accidental output path regression in a40071f0dadfb7af26c281f26e8aca5f0a42e5d3

AssetRipper.Core/Project/Collections/AssetExportCollection.cs
@@ -29,8 +29,8 @@ namespace AssetRipper.Core.Project.Collections
 					: Asset.OriginalName.Length > 0
 						? Asset.OriginalName
 						: Asset.AssetClassName;
-				string resourcePath = Path.GetFullPath(FileUtils.FixInvalidNameCharacters(
-					Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
+				string resourcePath = Path.Combine(projectDirectory, DirectoryUtils.FixInvalidPathCharacters(
+					Path.Combine(Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
 				subPath = Path.GetDirectoryName(resourcePath)!;
 				string resFileName = Path.GetFileName(resourcePath);
 				fileName = GetUniqueFileName(subPath, resFileName);
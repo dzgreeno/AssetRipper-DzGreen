dzgreeno/AssetRipper-DzGreen

commit 230d2478b712ee9900ea152f6980cab19a6032a0

Custom IGameInitializer implementation * Support changing Unity version * Support engine asset injection * Resolves #696 * Resolves #1271

Source/AssetRipper.Assets/Collections/AssetCollection.cs
@@ -114,7 +114,7 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 		return new PPtr<T>(fileIndex, asset.PathID);
 	}
 
-	private protected void AddAsset(IUnityObjectBase asset)
+	protected void AddAsset(IUnityObjectBase asset)
 	{
 		ValidateAsset(asset);
 
@@ -124,11 +124,40 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 		{
 			if (asset.Collection != this)
 			{
-				throw new ArgumentException("Asset info must marked this as its collection.", nameof(asset));
+				throw new ArgumentException("AssetInfo must have this marked as its collection.", nameof(asset));
 			}
 			if (asset.PathID is 0)
 			{
 				throw new ArgumentException("The zero path ID is reserved for null PPtr's.", nameof(asset));
+			}
+		}
+	}
+
+	/// <summary>
+	/// Replace an asset in this collection.
+	/// </summary>
+	/// <remarks>
+	/// This is useful for switching the underlying implementation, such as for version changing.
+	/// </remarks>
+	/// <param name="replacement"></param>
+	public void ReplaceAsset(IUnityObjectBase replacement)
+	{
+		ValidateAsset(replacement);
+		assets[replacement.PathID] = replacement;
+
+		void ValidateAsset(IUnityObjectBase replacement)
+		{
+			if (replacement.Collection != this)
+			{
+				throw new ArgumentException("AssetInfo must have this marked as its collection.", nameof(replacement));
+			}
+			if (!TryGetAsset(replacement.PathID, out IUnityObjectBase? original))
+			{
+				throw new ArgumentException("There is no existing asset with this PathID.", nameof(replacement));
+			}
+			if (replacement.ClassID != original.ClassID)
+			{
+				throw new ArgumentException("The replacement asset's class id is not equal to the original asset's class id.", nameof(replacement));
 			}
 		}
 	}
@@ -153,7 +182,7 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 
 	public bool TryGetAsset(long pathID, [NotNullWhen(true)] out IUnityObjectBase? asset)
 	{
-		return TryGetAsset<IUnityObjectBase>(pathID, out asset);
+		return assets.TryGetValue(pathID, out asset);
 	}
 
 	public bool TryGetAsset<T>(long pathID, [NotNullWhen(true)] out T? asset) where T : IUnityObjectBase

Source/AssetRipper.Assets/Collections/ProcessedAssetCollection.cs
@@ -1,4 +1,7 @@
 ﻿using AssetRipper.Assets.Bundles;
+using AssetRipper.Assets.Metadata;
+using AssetRipper.IO.Files;
+using AssetRipper.IO.Files.SerializedFiles;
 
 namespace AssetRipper.Assets.Collections;
 
@@ -7,8 +10,49 @@ namespace AssetRipper.Assets.Collections;
 /// </summary>
 public sealed class ProcessedAssetCollection : VirtualAssetCollection
 {
+	private long m_nextId;
+
 	public ProcessedAssetCollection(Bundle bundle) : base(bundle)
 	{
+	}
+
+	public new string Name
+	{
+		get => base.Name;
+		set => base.Name = value;
+	}
+
+	public void SetLayout(UnityVersion version)
+	{
+		SetLayout(version, BuildTarget.NoTarget, TransferInstructionFlags.NoTransferInstructionFlags);
+	}
+
+	public void SetLayout(UnityVersion version, BuildTarget platform, TransferInstructionFlags flags)
+	{
+		Version = version;
+		Platform = platform;
+		Flags = flags;
+	}
+
+	public T CreateAsset<T>(int classID, Func<AssetInfo, T> factory) where T : IUnityObjectBase
+	{
+		AssetInfo assetInfo = CreateAssetInfo(classID);
+		T asset = factory(assetInfo);
+		AddAsset(asset);
+		return asset;
+	}
+
+	public TAsset CreateAsset<TData, TAsset>(int classID, TData data, Func<AssetInfo, TData, TAsset> factory) where TAsset : IUnityObjectBase
+	{
+		AssetInfo assetInfo = CreateAssetInfo(classID);
+		TAsset asset = factory(assetInfo, data);
+		AddAsset(asset);
+		return asset;
+	}
+
+	private AssetInfo CreateAssetInfo(int classID)
+	{
+		return new AssetInfo(this, ++m_nextId, classID);
 	}
 
 	protected override bool IsCompatibleDependency(AssetCollection dependency)

Source/AssetRipper.Assets/Collections/VirtualAssetCollection.cs
@@ -13,45 +13,4 @@ public abstract class VirtualAssetCollection : AssetCollection
 	protected VirtualAssetCollection(Bundle bundle) : base(bundle)
 	{
 	}
-
-	public new string Name
-	{
-		get => base.Name;
-		set => base.Name = value;
-	}
-
-	public void SetLayout(UnityVersion version)
-	{
-		SetLayout(version, BuildTarget.NoTarget, TransferInstructionFlags.NoTransferInstructionFlags);
-	}
-
-	public void SetLayout(UnityVersion version, BuildTarget platform, TransferInstructionFlags flags)
-	{
-		Version = version;
-		Platform = platform;
-		Flags = flags;
-	}
-
-	public T CreateAsset<T>(int classID, Func<AssetInfo, T> factory) where T : IUnityObjectBase
-	{
-		AssetInfo assetInfo = CreateAssetInfo(classID);
-		T asset = factory(assetInfo);
-		AddAsset(asset);
-		return asset;
-	}
-
-	public TAsset CreateAsset<TData, TAsset>(int classID, TData data, Func<AssetInfo, TData, TAsset> factory) where TAsset : IUnityObjectBase
-	{
-		AssetInfo assetInfo = CreateAssetInfo(classID);
-		TAsset asset = factory(assetInfo, data);
-		AddAsset(asset);
-		return asset;
-	}
-
-	private AssetInfo CreateAssetInfo(int classID)
-	{
-		return new AssetInfo(this, ++m_nextId, classID);
-	}
-
-	private long m_nextId;
 }

Source/AssetRipper.Export.UnityProjects/EngineAssets/EngineAssetsExporter.cs
@@ -73,7 +73,12 @@ public class EngineAssetsExporter : IAssetExporter
 
 	public bool TryCreateCollection(IUnityObjectBase asset, [NotNullWhen(true)] out IExportCollection? exportCollection)
 	{
-		if (!IsEngineFile(asset.Collection.Name))
+		if (IsEngineFile(asset.Collection.Name, out UnityGuid engineGuid))
+		{
+			exportCollection = new SingleRedirectExportCollection(asset, asset.PathID, engineGuid, AssetType.Internal);
+			return true;
+		}
+		else
 		{
 			if (asset is IMaterial material)
 			{
@@ -126,9 +131,26 @@ public class EngineAssetsExporter : IAssetExporter
 		return false;
 	}
 
-	private static bool IsEngineFile(string? fileName)
+	private static bool IsEngineFile(string? fileName, out UnityGuid guid)
 	{
-		return FilenameUtils.IsDefaultResource(fileName) || FilenameUtils.IsBuiltinExtra(fileName) || FilenameUtils.IsEngineGeneratedF(fileName);
+		if (FilenameUtils.IsDefaultResource(fileName))
+		{
+			guid = PredefinedAssetCache.EGUID;
+			return true;
+		}
+		else if (FilenameUtils.IsBuiltinExtra(fileName))
+		{
+			guid = PredefinedAssetCache.FGUID;
+			return true;
+		}
+		else if (FilenameUtils.IsEngineGeneratedF(fileName))
+		{
+			//Not sure this is correct
+			guid = PredefinedAssetCache.FGUID;
+			return true;
+		}
+		guid = default;
+		return false;
 	}
 
 	public AssetType ToExportType(IUnityObjectBase asset)

Source/AssetRipper.Export.UnityProjects/EngineAssets/PredefinedAssetCache.cs
@@ -73,11 +73,11 @@ public sealed class PredefinedAssetCache
 	/// <summary>
 	/// The predefined guid for unity default resources
 	/// </summary>
-	private static UnityGuid EGUID { get; } = new UnityGuid(0x00000000, 0x00000000, 0x0000000E, 0x00000000);
+	internal static UnityGuid EGUID { get; } = new UnityGuid(0x00000000, 0x00000000, 0x0000000E, 0x00000000);
 	/// <summary>
 	/// The predefined guid for unity builtin extra
 	/// </summary>
-	private static UnityGuid FGUID { get; } = new UnityGuid(0x00000000, 0x00000000, 0x0000000F, 0x00000000);
+	internal static UnityGuid FGUID { get; } = new UnityGuid(0x00000000, 0x00000000, 0x0000000F, 0x00000000);
 
 	private readonly Dictionary<NamedObjectKey, AssetMetaPtr> namedObjectDictionary = new();
 	private readonly Dictionary<ComponentKey, AssetMetaPtr> componentDictionary = new();

Source/AssetRipper.GUI.Localizations/Localization.g.cs
@@ -1006,6 +1006,11 @@ partial class Localization
 	public static string Success => Get("success");
 	
 	/// <summary>
+	/// Target Version For Version Changing
+	/// </summary>
+	public static string TargetVersionForVersionChanging => Get("target_version_for_version_changing");
+	
+	/// <summary>
 	/// Terrain Export Format
 	/// </summary>
 	public static string TerrainExportTitle => Get("terrain_export_title");

Source/AssetRipper.GUI.Web/Pages/Settings/SettingsPage.cs
@@ -90,6 +90,14 @@ public sealed partial class SettingsPage : DefaultPage
 									WriteCheckBoxForEnableAssetDeduplication(writer, Localization.EnableAssetDeduplication);
 								}
 							}
+
+							using (new Div(writer).WithClass("row").End())
+							{
+								using (new Div(writer).WithClass("col").End())
+								{
+									WriteTextAreaForTargetVersion(writer);
+								}
+							}
 						}
 					}
 
@@ -184,6 +192,18 @@ public sealed partial class SettingsPage : DefaultPage
 			.WithId(nameof(Configuration.ImportSettings.DefaultVersion))
 			.WithName(nameof(Configuration.ImportSettings.DefaultVersion))
 			.WithValue(Configuration.ImportSettings.DefaultVersion.ToString())
+			.Close();
+	}
+
+	private static void WriteTextAreaForTargetVersion(TextWriter writer)
+	{
+		new Label(writer).WithClass("form-label").WithFor(nameof(Configuration.ImportSettings.TargetVersion)).Close(Localization.TargetVersionForVersionChanging);
+		new Input(writer)
+			.WithType("text")
+			.WithClass("form-control")
+			.WithId(nameof(Configuration.ImportSettings.TargetVersion))
+			.WithName(nameof(Configuration.ImportSettings.TargetVersion))
+			.WithValue(Configuration.ImportSettings.TargetVersion.ToString())
 			.Close();
 	}
 

Source/AssetRipper.GUI.Web/Pages/Settings/SettingsPage.g.cs
@@ -23,6 +23,9 @@ partial class SettingsPage
 			case nameof(ImportSettings.DefaultVersion):
 				Configuration.ImportSettings.DefaultVersion = TryParseUnityVersion(value);
 				break;
+			case nameof(ImportSettings.TargetVersion):
+				Configuration.ImportSettings.TargetVersion = TryParseUnityVersion(value);
+				break;
 			case nameof(ImportSettings.BundledAssetsExportMode):
 				Configuration.ImportSettings.BundledAssetsExportMode = TryParseEnum<BundledAssetsExportMode>(value);
 				break;

commit e09403be8391704a853c9f294500062b679bfd31

Fix asset resolution regression in 230d2478b712ee9900ea152f6980cab19a6032a0

Source/AssetRipper.Assets/Collections/AssetCollection.cs
@@ -151,7 +151,7 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 			{
 				throw new ArgumentException("AssetInfo must have this marked as its collection.", nameof(replacement));
 			}
-			if (!TryGetAsset(replacement.PathID, out IUnityObjectBase? original))
+			if (!assets.TryGetValue(replacement.PathID, out IUnityObjectBase? original))
 			{
 				throw new ArgumentException("There is no existing asset with this PathID.", nameof(replacement));
 			}
@@ -182,7 +182,7 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 
 	public bool TryGetAsset(long pathID, [NotNullWhen(true)] out IUnityObjectBase? asset)
 	{
-		return assets.TryGetValue(pathID, out asset);
+		return TryGetAsset<IUnityObjectBase>(pathID, out asset);
 	}
 
 	public bool TryGetAsset<T>(long pathID, [NotNullWhen(true)] out T? asset) where T : IUnityObjectBase
@@ -191,6 +191,7 @@ public abstract class AssetCollection : IReadOnlyCollection<IUnityObjectBase>
 		{
 			if (typeof(T).IsAssignableTo(typeof(NullObject)))
 			{
+				//T inherits from NullObject, so we allow the null object to be found.
 				switch (@object)
 				{
 					case T t: